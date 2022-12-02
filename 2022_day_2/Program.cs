ddde1079c1379e30a16d572762774b02ae0dba83
Reynold Tamayo <t452172@deluxe.com>
2022-12-02 17:09:35 -0500

    day 2 part 1 checkin
    part 1 checkin

--- 2022_day_2/Program.cs
+++ 2022_day_2/Program.cs
@@ -1,2 +1,117 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace FileApplication {
+   class Program {
+      static void Main(string[] args) {
+        string dataLocation = "";
+        
+        //data to use
+        //dataLocation = "/Users/T452172/Documents/Personal/Advent_of_Code/2022/AoC2022/2022_day_2/sampleData.txt";
+        dataLocation = "/Users/T452172/Documents/Personal/Advent_of_Code/2022/AoC2022/2022_day_2/aocData.txt"; 
+
+        //problem part
+        int partNum = 1;
+        //int partNum = 2;
+
+            try {
+                using (StreamReader sr = new StreamReader(dataLocation)) {
+                    int score = 0;
+                    string data;
+                    
+                    while ((data = sr.ReadLine()) != null) {
+                        Console.WriteLine("data: {0}", data);
+                        // string[] subs = line.Split(" ");
+                        // //separate per endpoint
+                        // string opponent = subs[0];
+                        // string mine = subs[1];                        
+
+                        //part 1 solution
+                        if (partNum == 1) {
+                            //find the combination
+                            //rock-rock; score + draw; 1 + 3
+                            if (data.CompareTo("A X") == 0) {
+                                score += 4;
+                            //rock-paper; score + won; 2 + 6
+                            } else if (data.CompareTo("A Y") == 0) {
+                                score += 8;
+                            //rock-scissor; score + lost; 3 + 0
+                            } else if (data.CompareTo("A Z") == 0) {
+                                score += 3;
+                            //paper-rock; score + lost; 1 + 0
+                            } else if (data.CompareTo("B X") == 0) {
+                                score += 1;
+                            //paper-paper; score + draw; 2 + 3
+                            } else if (data.CompareTo("B Y") == 0) {
+                                score += 5;
+                            //paper-scissor; score + won; 3 + 6
+                            } else if (data.CompareTo("B Z") == 0) {
+                                score += 9;
+                            //scissor-rock; score + won; 1 + 6
+                            } else if (data.CompareTo("C X") == 0) {
+                                score += 7;
+                            //scissor-paper; score + lost; 2 + 0
+                            } else if (data.CompareTo("C Y") == 0) {
+                                score += 2;
+                            //scissor-scissor; score + draw; 3 + 3
+                            } else if (data.CompareTo("C Z") == 0) {
+                                score += 6;
+                            }
+
+                        //part 2 solution
+                        } else if (partNum == 2) {                                
+                            // //set the first three as the top 3
+                            // //add them to a list and sort ascending
+                            // if (elfNum <= 3) {
+                            //     if (elfNum == 1) {
+                            //         calories[0] = currentCalories;
+                            //     } else if (elfNum == 2) {
+                            //         calories[1] = currentCalories;
+                            //     } else if (elfNum == 3) {
+                            //         calories[2] = currentCalories;
+                            //     }
+                            
+                            // } else {
+                            //     // foreach (double cal in calories) {
+                            //     //     Console.WriteLine("cal: {0}", cal);
+                            //     // }
+
+                            //     bool update = false;
+                            //     for (int cnt = 0; cnt < calories.Length; cnt++){
+                            //         double calData = calories[cnt];
+                            //         if (currentCalories.CompareTo(calData) == 1) {
+                            //             update = true;
+                            //         } 
+                            //     }  
+
+                            //     if (update) {
+                            //             //sort array and replace lowest with new data
+                            //             Array.Sort(calories);
+                            //             calories[0] = currentCalories;
+                            //     }                                   
+                            // }                           
+                        }
+                    }
+
+                    if (partNum == 1) {
+                        Console.WriteLine("score: {0}", score);
+                    } else if (partNum == 2) {                        
+                        // foreach (double cal in calories) {
+                        //     maxCalories += cal;
+                        // }
+                        // Console.WriteLine("Total calories of top 3 bags: {0}", maxCalories);
+                    }
+                }                    
+
+            } catch (Exception e) {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+      }
+   }
+}
