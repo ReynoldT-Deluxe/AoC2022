c72609c7644af147f1aaff99e649a4989132018b
Reynold Tamayo <t452172@deluxe.com>
2022-12-02 16:28:32 -0500

    day 1 part 2 checkin
    *checking for day 2 and removal of some unwanted code
    *had to update the data to add a blank after the last group since my check for a group to be considered as the contents of a bag was the space.  either that or create duplicate code for the final run

--- 2022_day_1/Program.cs
+++ 2022_day_1/Program.cs
@@ -7,55 +7,17 @@ using System.Collections.Specialized;
 
 namespace FileApplication {
    class Program {
-       public static double processData(int fishCnt, int checkDays, int lanternFishLife) {
-            int zeroCnt1; 
-            List<double> newFishList1 = new List<double>();
-            //Console.WriteLine("newFishList1.Count: {0}", newFishList1.Count);   
-            //Console.WriteLine("Fish count after 1 days: {0}", newFishList1.Count);
-
-            for (int day = 1; day <= checkDays; day++) {
-                zeroCnt1 = 0;
-                //set data for day 1
-                if (day == 1){
-                    newFishList1.Add(lanternFishLife);
-                }
-
-                double[] arr2 = new double[newFishList1.Count];
-                newFishList1.CopyTo(arr2); 
-                
-                for (int x = 0; x < arr2.Length; x++)  {
-                    double info1  =  new double();
-                    info1 = arr2[x];
-                    //check if zero count
-                    if (info1 != 0) {
-                        //set data to lower by 1
-                        info1--;
-                    } else if (info1 == 0) {
-                        //reset data to 6
-                        info1 = 6;
-                        zeroCnt1++;
-                    }
-                    //set the data back to the list
-                    arr2[x] = info1;
-                }
-
-                //convert array to list
-                newFishList1 = arr2.ToList();
-                //add a new lanternfish with 8 days
-                if (zeroCnt1 > 0) {
-                    for (int zC = 0; zC < zeroCnt1; zC++) {
-                        newFishList1.Add(8);
-                    }
-                }
-                Console.WriteLine("Fish count for fish {0} after {1} days: {2}", fishCnt, day, newFishList1.Count);
-            }
-
-           return newFishList1.Count;
-       }
       static void Main(string[] args) {
         string dataLocation = "";
+        
+        //data to use
         //dataLocation = "/Users/T452172/Documents/Personal/Advent_of_Code/2022/AoC2022/2022_day_1/sampleData.txt";
         dataLocation = "/Users/T452172/Documents/Personal/Advent_of_Code/2022/AoC2022/2022_day_1/aocData.txt"; 
+
+        //problem part
+        //int partNum = 1;
+        int partNum = 2;
+
             try {
                 using (StreamReader sr = new StreamReader(dataLocation)) {
                     string data;
@@ -64,33 +26,75 @@ namespace FileApplication {
                     double maxCalories = 0;
                     int elfNum = 1;
                     int elfNumMax = 1;
+                    double[] calories = new double[3];
 
                     while ((data = sr.ReadLine()) != null) {
-                        //Console.WriteLine("data: {0}", data);
                         //check data if it's blank 
                         if (data.Length != 0) {
                             currentCalories += Convert.ToDouble(data);
+
                         //means it is the end of the elf bag data
                         } else {
-                            if (currentCalories > maxCalories) {
-                                maxCalories = currentCalories;
-                                //set elfNum for maxCalories
-                                elfNumMax = elfNum;
-                            } 
+                            //part 1 solution
+                            if (partNum == 1) {
+                                if (currentCalories > maxCalories) {
+                                    maxCalories = currentCalories;
+                                    //set elfNum for maxCalories
+                                    elfNumMax = elfNum;
+                                } 
+
+                            //part 2 solution
+                            } else if (partNum == 2) {                                
+                                //set the first three as the top 3
+                                //add them to a list and sort ascending
+                                if (elfNum <= 3) {
+                                    if (elfNum == 1) {
+                                        calories[0] = currentCalories;
+                                    } else if (elfNum == 2) {
+                                        calories[1] = currentCalories;
+                                    } else if (elfNum == 3) {
+                                        calories[2] = currentCalories;
+                                    }
+                                
+                                } else {
+                                    // foreach (double cal in calories) {
+                                    //     Console.WriteLine("cal: {0}", cal);
+                                    // }
+
+                                    bool update = false;
+                                    for (int cnt = 0; cnt < calories.Length; cnt++){
+                                        double calData = calories[cnt];
+                                        if (currentCalories.CompareTo(calData) == 1) {
+                                           update = true;
+                                        } 
+                                    }  
+
+                                    if (update) {
+                                         //sort array and replace lowest with new data
+                                         Array.Sort(calories);
+                                         calories[0] = currentCalories;
+                                    }                                   
+                                }                           
+                            }
+
                             //increment elfNum
                             elfNum++;
                             //reset currentCalories
                             currentCalories = 0;
                         }
-                        
-                        //Console.WriteLine("elfNum: {0}", elfNum);
-                        //Console.WriteLine("elfNumMax: {0}", elfNumMax);
-                        //Console.WriteLine("currentCalories: {0}", currentCalories);
-                        //Console.WriteLine("maxCalories: {0}", maxCalories);
                     }
 
-                    Console.WriteLine("elfNumMax: {0}", elfNumMax);
-                    Console.WriteLine("maxCalories: {0}", maxCalories);
+                    if (partNum == 1) {
+                        Console.WriteLine("elfNumMax: {0}", elfNumMax);
+                        Console.WriteLine("maxCalories: {0}", maxCalories);
+                    } else if (partNum == 2) {                        
+                        foreach (double cal in calories) {
+                            maxCalories += cal;
+                        }
+                        Console.WriteLine("Total calories of top 3 bags: {0}", maxCalories);
+                    }
+
+                    
                               
                 }                    
 
