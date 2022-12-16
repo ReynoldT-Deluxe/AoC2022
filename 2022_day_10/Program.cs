4a48b7b42c25551b0e3613cacdfe1652f1bc034e
Reynold Tamayo <t452172@deluxe.com>
2022-12-15 22:34:09 -0500

    day 11 part 1 and 2 checkin
    * added partial solution to part 2 for day 10
    * solved part 1 for day 11
    * added solution for part 2 of day 11, but the total inspection count value was not returning correctly since the 'currentWorryLevel' values are reaching astronomical values, causing errors for the evaluation of the 'divisible by'

--- 2022_day_10/Program.cs
+++ 2022_day_10/Program.cs
@@ -32,8 +32,8 @@ namespace FileApplication
             //cycle thru inputs
             int sumOfSignalStregths = processProgramInputs(programInputs);
 
+            Console.WriteLine("");
             Console.WriteLine("The sum of the six signal strengths: {0}", sumOfSignalStregths);
-
         }
 
         public static List<ProgramInput> getProgramInputsFromFile(string dataLocation)
@@ -67,9 +67,16 @@ namespace FileApplication
 
         public static int processProgramInputs(List<ProgramInput> programInputs)
         {
+            //for part 1
             int signalStrength = 0;
             int index = 0;
             int registerValue = 1;
+            
+            //for part 2
+            int crtIndex = 0;
+            Sprite sprite = new Sprite(1);
+
+            //Console.WriteLine("sprite: {0},{1},{2}", sprite.position1, sprite.position2, sprite.position3);
 
             foreach (ProgramInput item in programInputs)
             {
@@ -78,6 +85,8 @@ namespace FileApplication
                 if (item.command.CompareTo("noop") == 0)
                 {
                     index++;
+                    //draw CRT output
+                    //crtIndex = drawCRTOutput(sprite, crtIndex);
                     //process signal strength for specified cycles
                     signalStrength += processSignalStrength(index, registerValue);
                 }
@@ -87,15 +96,20 @@ namespace FileApplication
                     for (int cnt = 1; cnt <= 2; cnt++)
                     {
                         index++;
+                        //draw CRT output
+                        //drawCRTOutput(sprite, crtIndex);
                         //process signal strength for specified cycles
-                        signalStrength += processSignalStrength(index, registerValue);
+                        signalStrength += processSignalStrength(index, registerValue);                        
 
                         if (cnt == 2)
                         {
                             registerValue += item.value;
+                            sprite.updateSprite(item.value);
+                            //Console.WriteLine("sprite: {0},{1},{2}", sprite.position1, sprite.position2, sprite.position3);
                         }
                     }
                 }
+
                 //Console.WriteLine("index: {0}\tregisterValue: {1}", index, registerValue);
             }
 
@@ -133,18 +147,129 @@ namespace FileApplication
 
             return signalStrength;
         }
-    }
 
-    public class ProgramInput
-    {
-        public string command { get; set; }
-        public int value { get; set; }
-        public int cycleCount { get; set; }
-        public ProgramInput(string command, int value, int cycleCount)
+        public static void drawTheCRTOutput(List<ProgramInput> programInputs)
         {
-            this.command = command;
-            this.value = value;
-            this.cycleCount = cycleCount;
+            int signalStrength = 0;
+            int index = 0;
+            int registerValue = 1;
+            int crtIndex = 0;
+            Sprite sprite = new Sprite(1);
+
+            foreach (ProgramInput item in programInputs)
+            {
+                //Console.WriteLine("command: {0}\tvalue: {1}", item.command, item.value);
+                //process noop
+                if (item.command.CompareTo("noop") == 0)
+                {
+                    index++;
+                    //process signal strength for specified cycles
+                    signalStrength += processSignalStrength(index, registerValue);
+                    //draw CRT output
+                    drawCRTOutput(sprite, crtIndex);
+                }
+                // process addx
+                else if (item.command.CompareTo("addx") == 0)
+                {
+                    for (int cnt = 1; cnt <= 2; cnt++)
+                    {
+                        index++;
+                        //process signal strength for specified cycles
+                        signalStrength += processSignalStrength(index, registerValue);
+                        drawCRTOutput(sprite, crtIndex);
+
+                        if (cnt == 2)
+                        {
+                            registerValue += item.value;
+                            sprite.updateSprite(item.value);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static bool isCRTIndexInSprite(int crtIndex, Sprite sprite)
+        {
+            if (crtIndex == sprite.position1 || crtIndex == sprite.position2 || crtIndex == sprite.position3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int drawCRTOutput(Sprite sprite, int crtIndex)
+        {
+            //draw CRT output
+            if (isCRTIndexInSprite(crtIndex, sprite))
+            {
+                if (crtIndex == 39)
+                {
+                    Console.WriteLine("#");
+                }
+                else
+                {
+                    Console.Write("#");
+                }
+            }
+            else
+            {
+                if (crtIndex == 39)
+                {
+                    Console.WriteLine(".");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+
+            //increment crtIndex
+            crtIndex++;
+
+            //reset to zero if the index is 40
+            if (crtIndex == 40)
+            {
+                crtIndex = 0;
+            }
+
+            return crtIndex;
+        }
+
+        public class ProgramInput
+        {
+            public string command { get; set; }
+            public int value { get; set; }
+            public int cycleCount { get; set; }
+            public ProgramInput(string command, int value, int cycleCount)
+            {
+                this.command = command;
+                this.value = value;
+                this.cycleCount = cycleCount;
+            }
+        }
+        public class Sprite
+        {
+            public int position1 { get; set; }
+            public int position2 { get; set; }
+            public int position3 { get; set; }
+            public Sprite(int data)
+            {
+                this.position1 = data - 1;
+                this.position2 = data;
+                this.position3 = data + 1;
+            }
+            public Sprite updateSprite(int value)
+            {
+                //Console.WriteLine("value: {0}", value);
+                this.position1 += value;
+                this.position2 += value;
+                this.position3 += value;
+
+                return this;
+            }
         }
     }
 }
---
+++ 2022_day_11/Program.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace FileApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //data to use
+            string dataLocation = "/Users/T452172/Documents/Personal/Advent_of_Code/2022/AoC2022/2022_day_11/sampleData.txt";
+            //string dataLocation = "/Users/T452172/Documents/Personal/Advent_of_Code/2022/AoC2022/2022_day_11/aocData.txt";
+
+            //int part = 1;
+            int part = 2;
+
+            List<Monkey> monkeyList = new List<Monkey>();
+            Monkey monkey = new Monkey();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(dataLocation))
+                {
+                    string data;
+                    int monkeyCount = 0;
+
+                    while ((data = sr.ReadLine()) != null)
+                    {
+                        string trimmedData = data.Trim();
+                        //Console.WriteLine(data.Trim());
+                        if (trimmedData.StartsWith("Monkey"))
+                        {
+                            //create a monkey
+                            monkey = new Monkey();
+                            //monkeyList.Add(monkey);
+
+                            string[] monkeyData = trimmedData.Split(":");
+                            string[] numberData = monkeyData[0].Split(" ");
+                            monkeyCount = int.Parse(numberData[1]);
+                            //Console.WriteLine("monkeyCount: {0}", monkeyCount);
+                        }
+                        else if (trimmedData.StartsWith("Operation"))
+                        {
+                            //get the operation and operationBy
+                            if (trimmedData.Contains("*"))
+                            {
+                                monkey.operation = "*";
+                            }
+                            else if (trimmedData.Contains("+"))
+                            {
+                                monkey.operation = "+";
+                            }
+
+                            string[] operationData = trimmedData.Split(monkey.operation);
+
+                            //operation is equal to old
+                            if (operationData[1].Trim().CompareTo("old") == 0)
+                            {
+                                //set to zero since we don't know what the old value is for now
+                                monkey.operationBy = 0;
+                            }
+                            else
+                            {
+                                monkey.operationBy = long.Parse(operationData[1].Trim());
+                            }
+                        }
+                        else if (trimmedData.StartsWith("Test"))
+                        {
+                            string[] testData = trimmedData.Split(" divisible by ");
+                            monkey.test = long.Parse(testData[1]);
+                        }
+                        else if (trimmedData.StartsWith("If true"))
+                        {
+                            string[] trueData = trimmedData.Split(" throw to monkey ");
+                            monkey.truthy = int.Parse(trueData[1]);
+                        }
+                        else if (trimmedData.StartsWith("If false"))
+                        {
+                            string[] falseData = trimmedData.Split(" throw to monkey ");
+                            monkey.falsy = int.Parse(falseData[1]);
+                        }
+                        else if (trimmedData.CompareTo("") == 0)
+                        {
+                            monkeyList.Add(monkey);
+                        }
+                        else if (trimmedData.StartsWith("Starting items"))
+                        {
+                            List<long> items = new List<long>();
+                            //get starting items
+                            string[] itemList = trimmedData.Split("Starting items: ");
+                            //Console.WriteLine("itemList[1]: {0}", itemList[1]);
+                            if (itemList[1].Contains(","))
+                            {
+                                //split the commas
+                                string[] item = itemList[1].Split(", ");
+                                foreach (string x in item)
+                                {
+                                    items.Add(long.Parse(x));
+                                }
+                            }
+                            else
+                            {
+                                items.Add(long.Parse(itemList[1]));
+                            }
+
+                            monkey.itemsWorryLevels = items;
+                        }
+                    }
+                    //add the last monkey data to the list
+                    monkeyList.Add(monkey);
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            //printMonkeyListData(monkeyList);
+            long levelOfMonkeyBusiness = 0;
+
+            //for part 1
+            if (part == 1)
+            {
+                levelOfMonkeyBusiness = processMonkeyList(monkeyList, false);
+                Console.WriteLine("Level of monkey business after 20 rounds of " +
+                    "stuff-slinging simian shenanigans (third of worry): {0}", levelOfMonkeyBusiness);
+            }
+
+            //for part 2
+            if (part == 2)
+            {
+                levelOfMonkeyBusiness = processMonkeyList(monkeyList, true);
+                Console.WriteLine("Level of monkey business after 10000 rounds of " +
+                    "stuff-slinging simian shenanigans (full worry): {0}", levelOfMonkeyBusiness);
+            }
+        }
+
+        public static void printMonkeyListData(List<Monkey> monkeyList)
+        {
+            //print monkey data
+            //Console.WriteLine(monkeyList.Count);
+            for (int count = 0; count < monkeyList.Count; count++)
+            {
+                //     Console.WriteLine("monkey {0} data", monkeyList.IndexOf(monkeyData));
+                //     Console.WriteLine("operation: {0}", monkeyData.operation);
+                //     Console.WriteLine("operationBy: {0}", monkeyData.operationBy);
+                //     Console.WriteLine("test: {0}", monkeyData.test);
+                //     Console.WriteLine("truthy: {0}", monkeyData.truthy);
+                //     Console.WriteLine("falsy: {0}", monkeyData.falsy);
+
+                Console.Write("Monkey {0}: ", count);
+                foreach (long item in monkeyList[count].itemsWorryLevels)
+                {
+                    Console.Write("{0} ", item);
+                }
+                Console.WriteLine("");
+                // Console.WriteLine("inspectionCount: {0}", monkeyList[count].inspectionCount);
+            }
+        }
+
+        public static int processMonkeyList(List<Monkey> monkeyList, bool fullLevelOfWorry)
+        {
+            int maxRound = 20;
+            //iterate thru 20 rounds
+            if (fullLevelOfWorry) {
+                maxRound = 10000;
+            }
+            
+            for (int round = 0; round < maxRound; round++)
+            {
+                foreach (Monkey monkey in monkeyList)
+                {
+                    while (monkey.itemsWorryLevels.Count != 0)
+                    {
+                        //calculate new Worry = x (old) operation y (operationBy)
+                        long x = monkey.itemsWorryLevels[0];
+                        long y = monkey.operationBy;
+                        if (monkey.operationBy == 0)
+                        {
+                            y = x;
+                        }
+
+                        long updatedWorry = 0;
+
+                        if (monkey.operation.Equals("*"))
+                        {
+                            updatedWorry = x * y;
+                        }
+                        else if (monkey.operation.Equals("+"))
+                        {
+                            updatedWorry = x + y;
+                        }
+
+                        //divide worry level by 3 and return the current worry level without the remainder
+                        long currentWorryLevel = updatedWorry;
+                        if (!fullLevelOfWorry)
+                        {
+                            currentWorryLevel = (long)Math.Floor((double)updatedWorry / 3);
+                        }
+
+                        //test divisibility
+                        //divisible
+                        if ((currentWorryLevel % monkey.test) == 0)
+                        {
+                            //truthy
+                            monkeyList[monkey.truthy].itemsWorryLevels.Add(currentWorryLevel);
+                        }
+                        else
+                        {
+                            //falsy
+                            monkeyList[monkey.falsy].itemsWorryLevels.Add(currentWorryLevel);
+                        }
+
+                        //remove from original
+                        monkey.itemsWorryLevels.RemoveAt(0);
+                        //increment the inspection count
+                        monkey.inspectionCount++;
+                    }
+                }
+
+                // Console.WriteLine("Round {0}", round + 1);
+                // printMonkeyListData(monkeyList);
+            }
+
+            return getLevelOfMonkeyBusiness(monkeyList);
+        }
+
+        public static int getLevelOfMonkeyBusiness(List<Monkey> monkeyList)
+        {
+            //sort descending by inspection count
+            List<Monkey> sortedListByInspectionCountDescending = monkeyList.OrderByDescending(info => info.inspectionCount).ToList();
+            //get the product of the top two
+            // Console.WriteLine("{0}, {1}", sortedListByInspectionCountDescending[0].inspectionCount, 
+            //     sortedListByInspectionCountDescending[1].inspectionCount);
+            return sortedListByInspectionCountDescending[0].inspectionCount * sortedListByInspectionCountDescending[1].inspectionCount;
+        }
+
+        public class Monkey
+        {
+            public List<long> itemsWorryLevels { get; set; }
+            public string operation { get; set; }
+            public long operationBy { get; set; }
+            public long test { get; set; }
+            public int truthy { get; set; }
+            public int falsy { get; set; }
+            public int inspectionCount { get; set; }
+        }
+    }
+}
